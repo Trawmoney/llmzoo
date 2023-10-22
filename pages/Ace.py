de09d169e5373e94bc4b01e3e4133b721cc02093
Neuromancer <79415352+Trawmoney@users.noreply.github.com>
2023-10-22 05:37:33 -0400

    Update Ace.py

--- pages/Ace.py
+++ pages/Ace.py
@@ -2,3 +2,7 @@ import streamlit as st
 from code_editor import code_editor
 
 response_dict = code_editor(your_code_string)
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
