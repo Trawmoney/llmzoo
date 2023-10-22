eeea2ac7687d55e0d773e6d200a9d36208a341a4
Neuromancer <79415352+Trawmoney@users.noreply.github.com>
2023-10-22 05:49:19 -0400

    Create constants.py

---
+++ shared/constants.py
@@ -0,0 +1,7 @@
+# Set to your app's URL
+OPENROUTER_REFERRER = "https://github.com/Trawmoney/llmzoo"
+# OPENROUTER_BASE = "http://localhost:3000"
+OPENROUTER_BASE = "https://openrouter.ai"
+OPENROUTER_API_BASE = f"{OPENROUTER_BASE}/api/v1"
+OPENROUTER_DEFAULT_CHAT_MODEL = "openai/gpt-3.5-turbo-16k"
+OPENROUTER_DEFAULT_INSTRUCT_MODEL = "anthropic/claude-v1-100k"
